e51ffaf3d566026edbfecf608ff6e09edc4ff3df
Rohan Duraipandian <personaldurp@gmail.com>
2024-08-25 19:43:53 -0400

    Initial commit for React tutorial

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+function HelloWorld() {
+  return <h1 className="greeting">Hello, world!</h1>;
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>
 );
 
